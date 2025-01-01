7e1cd29592d783aae5ab47b812b32a88f36e05a7
Kori1167 <pawel_sz@10g.pl>
2025-01-01 18:44:37 +0100

    formularz, nazwa

--- formularz.php
+++ formularz.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PLACEHOLDER!!!!!!!!</title>
+    <title>Stardew Valley</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="styl.css">
 </head>
@@ -14,12 +14,14 @@
             <form method="post" action="postac.php">
                 <div class="row">
                     <div class="col-5">
-                        <div class="img_character">
-                            
-                        </div>
+                        <div class="img_character"></div>
+                        <label for="w">w</label>
+                        <input type="radio" id="w" name="gender">
+                        <label for="m">m</label>
+                        <input type="radio" id="m" name="gender">
                     </div>
                     <div class="col-7">
-
+                        
                     </div>
                 </div>
             </form>
